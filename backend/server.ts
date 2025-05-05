a0c8796e36d46d16ea4dd52f6678ae1a457de5ec
sozenka <yasserhoriarts@gmail.com>
2025-05-05 16:00:24 -0400

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,16 @@ dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://h25-sim-eq4.vercel.app/' // replace with your actual frontend URL
+  ],
+  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // ✅ MongoDB connection
